ec5bb271dd177b4280fb88041424f315b2fe9e0f
ForceScripts <71199348+IDKFORCE@users.noreply.github.com>
2023-06-05 12:04:40 -0700

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,9 +1,19 @@
-RegisterServerEvent('fs-slaping:SyncOn_Server')
-AddEventHandler('fs-slaping:SyncOn_Server', function()
-    TriggerClientEvent('fs-slaping:SyncOn_Client', -1, source)
+local TriggerClientEvent = TriggerClientEvent
+local RegisterServerEvent = RegisterServerEvent
+local AddEventHandler = AddEventHandler
+
+local SYNC_ON_SERVER = 'fs-slaping:SyncOn_Server'
+local SYNC_ON_CLIENT = 'fs-slaping:SyncOn_Client'
+local SYNC_GIFFLE = 'fs-slaping:SyncGiffle'
+local SYNC_ANIMATION = 'fs-slaping:SyncAnimation'
+
+RegisterServerEvent(SYNC_ON_SERVER)
+RegisterServerEvent(SYNC_GIFFLE)
+
+AddEventHandler(SYNC_ON_SERVER, function()
+    TriggerClientEvent(SYNC_ON_CLIENT, -1, source)
 end)
 
-RegisterServerEvent('fs-slaping:SyncGiffle')
-AddEventHandler('fs-slaping:SyncGiffle', function(netID)
-    TriggerClientEvent('fs-slaping:SyncAnimation', netID)
+AddEventHandler(SYNC_GIFFLE, function(netID)
+    TriggerClientEvent(SYNC_ANIMATION, netID)
 end)
